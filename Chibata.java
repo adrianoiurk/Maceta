b6582fc0b728693167248b7aa2ef7547d1f74863
adriano <adriano.iurk@gmail.com>
2013-04-01 21:52:45 -0300

    Msg

--- Chibata.java
+++ Chibata.java
@@ -2,6 +2,7 @@ public class Chibata{
 	public static void main(String[] args){
 		System.out.println("Hello world!!");
 		System.out.println("github alteracao master!!");
+		System.out.println("CHIBATA NO BALDE!!");
 	}
 
 }
